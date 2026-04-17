6baf1dbe9ad1901b9afab359a560e67d65b3b461
bjfanxc <bjfanxc@163.com>
2026-04-17 12:34:19 +0800

    init

--- monitor-ui/src/layout/components/Sidebar/Logo.vue
+++ monitor-ui/src/layout/components/Sidebar/Logo.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="sidebar-logo-container" :class="{'collapse':collapse}" :style="{ backgroundColor: sideTheme === 'theme-dark' && navType !== 3 ? variables.menuBackground : variables.menuLightBackground }">
+  <div
+    class="sidebar-logo-container"
+    :class="{ collapse }"
+    :style="{ backgroundColor: sideTheme === 'theme-dark' && navType !== 3 ? variables.menuBackground : variables.menuLightBackground }"
+  >
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link is-collapse" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 v-else class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
+        <div class="sidebar-brand">
+          <h1
+            class="sidebar-title"
+            :style="{ color: sideTheme === 'theme-dark' && navType !== 3 ? variables.logoTitleColor : variables.logoLightTitleColor }"
+          >
+            {{ title }}
+          </h1>
+          <p class="sidebar-subtitle">Monitor Console</p>
+        </div>
       </router-link>
     </transition>
   </div>
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.png'
+import logoImg from '@/assets/logo/monitor-mark.svg'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
@@ -47,7 +58,7 @@ export default {
 
 <style lang="scss" scoped>
 .sidebarLogoFade-enter-active {
-  transition: opacity 1.5s;
+  transition: opacity 0.8s;
 }
 
 .sidebarLogoFade-enter,
@@ -57,38 +68,66 @@ export default {
 
 .sidebar-logo-container {
   position: relative;
-  height: 50px;
-  line-height: 50px;
-  background: #2b2f3a;
-  text-align: center;
+  height: 64px;
+  background: #1a1f2e;
   overflow: hidden;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.06);
 
-  & .sidebar-logo-link {
+  .sidebar-logo-link {
+    display: flex !important;
+    align-items: center;
+    justify-content: flex-start;
+    flex-wrap: nowrap;
     height: 100%;
     width: 100%;
+    padding: 0 22px;
+    text-decoration: none;
+  }
 
-    & .sidebar-logo {
-      width: 32px;
-      height: 32px;
-      vertical-align: middle;
-      margin-right: 12px;
-    }
+  .sidebar-logo-link.is-collapse {
+    justify-content: center;
+    padding: 0;
+  }
 
-    & .sidebar-title {
-      display: inline-block;
-      margin: 0;
-      color: #fff;
-      font-weight: 600;
-      line-height: 50px;
-      font-size: 14px;
-      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-      vertical-align: middle;
-    }
+  .sidebar-logo {
+    width: 38px;
+    height: 38px;
+    flex: 0 0 auto;
+    display: block;
+  }
+
+  .sidebar-brand {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-width: 0;
+    margin-left: 12px;
+  }
+
+  .sidebar-title {
+    margin: 0;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 1.1;
+    letter-spacing: 0.04em;
+    color: #fff;
+    white-space: nowrap;
+  }
+
+  .sidebar-subtitle {
+    margin: 4px 0 0;
+    font-size: 11px;
+    line-height: 1;
+    letter-spacing: 0.22em;
+    text-transform: uppercase;
+    color: rgba(255, 255, 255, 0.56);
+    white-space: nowrap;
   }
 
   &.collapse {
     .sidebar-logo {
-      margin-right: 0px;
+      width: 34px;
+      height: 34px;
     }
   }
 }
