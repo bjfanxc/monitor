1394b9c8b36a3b488ff9d7907fe74df60b07cd36
bjfanxc <bjfanxc@163.com>
2026-04-19 23:24:13 +0800

    去掉默认用户名密码

--- monitor-ui/vue.config.js
+++ monitor-ui/vue.config.js
@@ -9,7 +9,7 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 const name = process.env.VUE_APP_TITLE || '监控管理系统' // 网页标题
 
-const baseUrl = 'http://localhost:8080' // 后端接口
+const baseUrl = 'https://api.monitorapps.vip' // 后端接口
 
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
